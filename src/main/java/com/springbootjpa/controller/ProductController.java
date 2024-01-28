71d7db309508516dc75ea7e9ab46c68df97388c8
Shantanu Ray <shantanu@sundewsolutions.com>
2024-01-28 23:28:20 +0530

    ResponseEntity added

--- src/main/java/com/springbootjpa/controller/ProductController.java
+++ src/main/java/com/springbootjpa/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.springbootjpa.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,45 +24,45 @@ public class ProductController {
     private ProductService service;
 
     @PostMapping("/addProduct")
-    public Product addProduct(@RequestBody Product product) {
-        return service.saveProduct(product);
+    public ResponseEntity<Object> addProduct(@RequestBody Product product) {
+        return new ResponseEntity(service.saveProduct(product), HttpStatus.OK);
     }
 
     @PostMapping("/addProducts")
-    public List<Product> addProducts(@RequestBody List<Product> products) {
-        return service.saveProducts(products);
+    public ResponseEntity<Object> addProducts(@RequestBody List<Product> products) {
+        return new ResponseEntity(service.saveProducts(products), HttpStatus.OK); 
     }
 
     @GetMapping("/products")
-    public List<Product> findAllProducts() {
-        return service.getAllProducts();
+    public ResponseEntity<Object> findAllProducts() {
+        return new ResponseEntity(service.getAllProducts(), HttpStatus.OK); 
     }
     
     @GetMapping("/products/{price}")
-    public List<Product> findSelectedProducts(@PathVariable Double price) {
-        return service.getSelectedProducts(price);
+    public ResponseEntity<Object> findSelectedProducts(@PathVariable Double price) {
+        return new ResponseEntity(service.getSelectedProducts(price), HttpStatus.OK);  
     }
     
     @GetMapping("/products2/{gprice}/{lprice}")
-    public List<Product> findSelectedProducts2(@PathVariable Double gprice, @PathVariable Double lprice) {
+    public ResponseEntity<Object> findSelectedProducts2(@PathVariable Double gprice, @PathVariable Double lprice) {
     	System.out.println(gprice);
     	System.out.println(lprice);
-        return service.getSelectedProducts2(gprice,lprice);
+        return new ResponseEntity(service.getSelectedProducts2(gprice,lprice), HttpStatus.OK) ;
     }
 
     @GetMapping("/productById/{id}")
-    public Product findProductById(@PathVariable int id) {
-        return service.getProductById(id);
+    public ResponseEntity<Object> findProductById(@PathVariable int id) {
+        return new ResponseEntity(service.getProductById(id), HttpStatus.OK)  ;
     }
 
     @GetMapping("/product/{name}")
-    public Product findProductByName(@PathVariable String name) {
-        return service.getProductByName(name);
+    public ResponseEntity<Object> findProductByName(@PathVariable String name) {
+        return new ResponseEntity(service.getProductByName(name), HttpStatus.OK) ;
     }
 
     @PutMapping("/update")
-    public Product updateProduct(@RequestBody Product product) {
-        return service.updateProduct(product);
+    public ResponseEntity<Object> updateProduct(@RequestBody Product product) {
+        return new ResponseEntity(service.updateProduct(product), HttpStatus.OK) ;
     }
 
     @DeleteMapping("/delete/{id}")
